8396cecfb4a76695477a1aab56670688388281bc
Victor adriel barbosa pereira <vabpvictor@gmail.com>
2023-03-03 19:26:39 -0300

    Desenvolvendo a autenticação

---
+++ backend/src/api/user/authService.js
@@ -0,0 +1,19 @@
+const _ = require('lodash')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const User = require('./user')
+const env = require('../../.env')
+
+const emailRegex = /\S+@\S+\.\S+/
+const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/
+
+const sendErrosFromDB = (res, dbErrors) => {
+    const errors = []
+    _.forIn(dbErrors.errors, error => errors.push(error.message))
+    return res.status(400).json({ errors })
+}
+
+const login = (req, res, next) => {
+    const email = req.body.email || ''
+    const password = req.body.password || ''
+}
